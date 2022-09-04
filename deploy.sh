29c3619609e21fc9608921983150fe9a5e1811fe
trisBouden <tris.bouden@mail.com>
2022-09-04 12:06:59 +0100

    deploy testing

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:trisDeveloper/dashboard.git master:gh-pages
+git push -f git@github.com:trisDeveloper/dashboard.git main:gh-pages
 cd -
